312939bd7d2721f53967ea9dfde1717e4e980d3d
Jason Griffin <jason@magnetic.io>
2020-10-21 20:02:07 +0200

    Check contour rolls out in under 5 mins

--- contour.sh
+++ contour.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # install contour ingress
-kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml && \
+  kubectl rollout status deployments/contour --timeout=300s --namespace=projectcontour
+
 
 # configure prometheus
 kubectl apply -f contour/prometheus/
