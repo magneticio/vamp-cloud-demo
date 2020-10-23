6fc3f1f95966ebf486e8624fde4462eee0195fe7
Jason Griffin <jason@magnetic.io>
2020-10-23 23:20:26 +0200

    Changed to use Contour v1.8.2

--- contour.sh
+++ contour.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install contour ingress
-kubectl apply -f contour/contour.yaml && \
+kubectl apply -f contour/contour/ && \
   kubectl rollout status deployments/contour --timeout=300s --namespace=projectcontour
 
 
