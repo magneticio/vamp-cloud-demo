3c4949179e471754896bdb23b1fe4c67153763f5
Jason Griffin <jason@magnetic.io>
2020-10-28 00:47:56 +0100

    Initial commit

---
+++ particles-optimiser/production/external-dns-shim-ingress.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+# get envoy load balancer address
+envoy_lb_addr=$(kubectl get service envoy --namespace=projectcontour -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+cat <<EOM | kubectl apply -f -
+apiVersion: vamp.io/v1
+kind: IngressShim
+metadata:
+  name: external-dns-shim
+  namespace: production
+spec:
+  annotations:
+    external-dns.alpha.kubernetes.io/target: $envoy_lb_addr
+EOM
