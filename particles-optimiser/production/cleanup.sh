5923b35eb2414f8af4213ebd04c40a053796e951
Jason Griffin <jason@magnetic.io>
2020-11-04 22:13:14 +0100

    Added --keep-agent

--- particles-optimiser/production/cleanup.sh
+++ particles-optimiser/production/cleanup.sh
@@ -4,4 +4,6 @@ kubectl delete -f particles-v1.0.0.yaml
 kubectl delete -f particles-v1.0.1.yaml
 kubectl delete -f particles-v1.0.2.yaml
 
-kubectl delete ns production
+if [ "$1" != "--keep-agent" ]; then
+    kubectl delete ns production
+fi
